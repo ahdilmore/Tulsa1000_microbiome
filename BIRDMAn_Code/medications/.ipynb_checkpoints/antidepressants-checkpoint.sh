e001662e4156b5ef371d3931f7f3381a7ca8dedf
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2024-03-10 17:01:09 -0700

    add birdman code

---
+++ BIRDMAn_Code/medications/.ipynb_checkpoints/antidepressants-checkpoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --chdir=/projects/u19/Tulsa/birdman/medications/
+#SBATCH --output=/projects/u19/Tulsa/birdman/medications/slurm_out/%x.%a.out
+#SBATCH --partition=short
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6:00:00
+#SBATCH --array=1-20
+
+pwd; hostname; date
+
+set -e
+
+source ~/anaconda3/bin/activate birdman
+
+echo Chunk $SLURM_ARRAY_TASK_ID / $SLURM_ARRAY_TASK_MAX
+
+TABLEID="medication_birdman_table"
+TABLE="/projects/u19/Tulsa/final_tables/"$TABLEID".biom"
+OUTDIR="/projects/u19/Tulsa/birdman/medications/inferences/"$TABLEID
+LOGDIR="/projects/u19/Tulsa/birdman/medications/logs/"$TABLEID
+mkdir -p $OUTDIR
+mkdir -p $LOGDIR
+
+echo Starting Python script...
+time python /projects/u19/Tulsa/birdman/src/antidepressants_birdman_chunked.py \
+    --table-path $TABLE \
+    --inference-dir $OUTDIR \
+    --num-chunks $SLURM_ARRAY_TASK_MAX \
+    --chunk-num $SLURM_ARRAY_TASK_ID \
+    --logfile "${LOGDIR}/chunk_${SLURM_ARRAY_TASK_ID}.log" && echo Finished Python script!
